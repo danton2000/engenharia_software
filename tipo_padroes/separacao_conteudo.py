5cf1729f8c9638139bb76bf0d45b522ad20e849a
DANTON DE CAMPOS RODRIGUES <181810032@fspoa.br>
2024-03-21 21:52:07 -0300

    Subindo alteracao no separacao_conteudo

--- tipo_padroes/separacao_conteudo.py
+++ tipo_padroes/separacao_conteudo.py
@@ -1,40 +1,45 @@
 # Verificando nome
-nome = ''
-while len(nome) < 3:
-    nome = input('Nome: ')
+def valida_nome():
+    nome = ''
+    while len(nome) < 3:
+        nome = input('Nome: ')
 
-# Verificando idade    
-idade = -1 
-while (idade < 0) or (idade > 150):
+# Verificando idade
+def valida_idade():    
+    idade = -1 
+    while (idade < 0) or (idade > 150):
 
-    try:
-        idade = int(input('Idade: '))
+        try:
+            idade = int(input('Idade: '))
 
-    except ValueError:
-        #print("Valor informado é invalido!, Digite um valor entre 0 e 150")
-        pass
-        #raise SystemExit
+        except ValueError:
+            #print("Valor informado é invalido!, Digite um valor entre 0 e 150")
+            pass
+            #raise SystemExit
         
 # Verificando Salário
 salario = -1
-while salario <= 0:
-    try:
-        salario = float(input('Salário: '))
-        
-    except ValueError:
-        #print("Valor informado é invalido!, Digite um salario maior que 0")
-        pass
-        #raise SystemExit
+def valida_salario():
+    while salario <= 0:
+        try:
+            salario = float(input('Salário: '))
+            
+        except ValueError:
+            #print("Valor informado é invalido!, Digite um salario maior que 0")
+            pass
+            #raise SystemExit
 
 # Verificando Sexo
-sexo = ''
-while (sexo != 'F') and (sexo != 'M'):
-    sexo = input('Sexo (F/M): ')
+def valida_sexo():
+    sexo = ''
+    while (sexo != 'F') and (sexo != 'M'):
+        sexo = input('Sexo (F/M): ')
 
 # Verificando Estado
-estado_civil = ''
-while ((estado_civil != 'S') and
-       (estado_civil != 'C') and
-       (estado_civil != 'V') and
-       (estado_civil != 'D')):
-       estado_civil = input('Estado Civil (S/C/V/D): ')
+def valida_estado_civil():
+    estado_civil = ''
+    while ((estado_civil != 'S') and
+        (estado_civil != 'C') and
+        (estado_civil != 'V') and
+        (estado_civil != 'D')):
+        estado_civil = input('Estado Civil (S/C/V/D): ')
